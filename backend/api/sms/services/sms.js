484fc0fd4128fecd92b76daa671ff2478fbdfcfd
Alex Kulikovskikh <alex.kulikovskikh@gmail.com>
2020-02-05 14:22:01 -0500

    feat(backend): Update phone verification service
    Ability to verify phone number using Firebase added. Session verification data is stored in Redis.

--- backend/api/sms/services/sms.js
+++ backend/api/sms/services/sms.js
@@ -1,3 +1,5 @@
+const NAMESPACE = 'accounts:verifications:mobilePhone:';
+const { HOUR_1 } = require('../../constants');
 const googleApis = require('googleapis');
 
 class SMS {
@@ -16,11 +18,49 @@ class SMS {
     });
   }
 
-  verify(verificationCode, phoneSessionId) {
-    return this.identityToolkit.relyingparty.verifyPhoneNumber({
-      verificationCode,
-      phoneSessionId,
-    });
+  saveSession({ phoneNumber, sessionInfo }) {
+    return strapi.services.redis.set(`${NAMESPACE}${phoneNumber}`, sessionInfo, 'EX', HOUR_1);
+  }
+
+  getSession(phoneNumber) {
+    return strapi.services.redis.get(`${NAMESPACE}${phoneNumber}`);
+  }
+
+  removeSession(phoneNumber) {
+    return strapi.services.redis.del(`${NAMESPACE}${phoneNumber}`);
+  }
+
+  async verify({ verificationCode, mobilePhone }) {
+    try {
+      const phoneSessionId = await strapi.services.sms.getSession(mobilePhone);
+      console.log('phoneSessionId', phoneSessionId);
+      if (!phoneSessionId) return false;
+
+      const verification = await this.identityToolkit.relyingparty.verifyPhoneNumber({
+        code: verificationCode,
+        sessionInfo: phoneSessionId,
+      });
+
+      console.log('verification', verification);
+
+      if (verification.status === 200) {
+        await strapi.services.sms.removeSession(mobilePhone);
+      }
+
+      return {
+        code: verification.status,
+        status: verification.statusText,
+      };
+
+    } catch(e) {
+      console.error(e);
+      strapi.log.error('sms.services.verify Error: %s', e.message);
+      return {
+        code: e.code,
+        error: e.message,
+      };
+    }
+
   }
 
 }
