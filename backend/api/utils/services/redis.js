057ab7cdbc8d9abb1b767e4cbd3de6e9f91bc10d
Alex Kulikovskikh <alex.kulikovskikh@gmail.com>
2020-04-09 14:41:02 -0700

    feat(backend): add ability to retrive multiple keys from redis store

--- backend/api/utils/services/redis.js
+++ backend/api/utils/services/redis.js
@@ -5,18 +5,26 @@
  */
 module.exports = {
 
-  get: async key => {
-
-    const data = await strapi.connections.redis.get(key);
-
+  parse: data => {
     if (!data) return false;
-
     try {
+      if (Array.isArray(data)) {
+        return data.map(el => JSON.parse(el));
+      }
       return JSON.parse(data);
     } catch (e) {
       return data;
     }
+  },
 
+  mget: async (...keys) => {
+    const data = await strapi.connections.redis.mget(...keys);
+    return strapi.services.redis.parse(data);
+  },
+
+  get: async key => {
+    const data = await strapi.connections.redis.get(key);
+    return strapi.services.redis.parse(data);
   },
 
   set: (key, value, optionName, optionValue) => {
