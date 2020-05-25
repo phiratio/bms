278f34710b4435c7bd405c0a5fa63a0e7a99f49d
Alex Kulikovskikh <alex.kulikovskikh@gmail.com>
2020-05-24 19:11:39 -0700

    feat(frontend): Add Settings UI components
    Add ability to:
    - Create/Remove Flic buttons
    - Update Slack settings
    - Update Terms and Conditions
    - Store information

--- frontends/apps/admin/routes/settings/Settings.js
+++ frontends/apps/admin/routes/settings/Settings.js
@@ -13,6 +13,9 @@ import {
   ModalBody,
   ModalHeader,
   Alert,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
 } from 'reactstrap';
 import s from './Settings.css';
 import ScrollingEmployees from '../../../../components/ScrollingEmployees';
@@ -22,13 +25,41 @@ import ToggleSettingDropdown from '../../../../components/ToggleSettingDropdown'
 import ReloadButton from '../../../../components/ReloadButton';
 import GeneralSettings from '../../../../components/Forms/GeneralSettingsForm';
 import ToggleSettingText from '../../../../components/ToggleSettingText';
+import EditorModal from '../../../../components/EditorModal';
+
+const FlicAction = ({ actionText, url, token }) => (
+  <InputGroup className="mb-2">
+    <InputGroupAddon addonType="prepend">
+      <InputGroupText>{actionText}</InputGroupText>
+    </InputGroupAddon>
+    <input
+      disabled
+      className="form-control"
+      value={`${window.App.apiUrl}${url}/?token=${token}`}
+    />
+    <InputGroupAddon
+      addonType="append"
+      onClick={() =>
+        navigator.clipboard.writeText(
+          `${window.App.apiUrl}${url}/?token=${token}`,
+        )
+      }
+    >
+      <InputGroupText>
+        <i className="icon-docs" />
+      </InputGroupText>
+    </InputGroupAddon>
+  </InputGroup>
+);
 
 class Settings extends React.Component {
   state = {
+    terms: '',
     loading: false,
     fatchFailed: false,
     modal: {
       employees: [],
+      token: '',
       selectedEmployee: [],
       originalEmployee: '',
       error: '',
@@ -36,6 +67,9 @@ class Settings extends React.Component {
     },
     employeeUpdateModal: false,
     settings: {
+      slack: {
+        config: {},
+      },
       queue: {},
       timeRanges: {
         from_5min_to_20min: [],
@@ -50,6 +84,10 @@ class Settings extends React.Component {
       waitinglist: {},
       generalSettings: {
         workingHours: {},
+        storeInfo: {},
+        socials: {},
+        address: {},
+        terms: '',
       },
       buttons: {
         data: [],
@@ -80,6 +118,7 @@ class Settings extends React.Component {
             })),
           },
         });
+        return true;
       })
       .catch(e => {
         if (e instanceof TypeError) {
@@ -89,13 +128,28 @@ class Settings extends React.Component {
         e._error && this.context.showNotification(e._error, 'error');
       });
 
+  addFlicButton = () =>
+    this.context.httpClient
+      .sendData('/buttons/', 'POST')
+      .then(validate.bind(this))
+      .then(() => this.getSettings());
+
+  deleteFlic = id =>
+    this.context.httpClient
+      .sendData(`/buttons/${id}`, 'DELETE')
+      .then(validate.bind(this))
+      .then(() => {
+        this.setState({
+          employeeUpdateModal: !this.state.employeeUpdateModal,
+        });
+        this.getSettings();
+      });
+
   saveSettings = (values, route = `/settings`) =>
     this.context.httpClient
       .sendData(route, 'PUT', values)
       .then(validate.bind(this))
-      .then(data => {
-        this.getSettings();
-      })
+      .then(() => this.getSettings())
       .catch(e => {
         if (Object.keys(e).length > 0 && typeof e.message !== 'string') {
           Object.keys(e).forEach(key => {
@@ -106,6 +160,9 @@ class Settings extends React.Component {
 
   saveGeneralSettings = values =>
     this.saveSettings(values, `/settings/general`);
+
+  saveTerms = values =>
+    this.saveSettings(values, `/settings/terms`);
 
   toggleSetting = (settingName, settingKey, value = false) =>
     this.context.httpClient
@@ -144,6 +201,7 @@ class Settings extends React.Component {
         modal: {
           ...this.state.modal,
           error: '',
+          token: props.token,
           ...(props.name && { header: props.name }),
           ...(props.username && { id: props.username }),
           ...(props.id && { id: props.id }),
@@ -191,7 +249,6 @@ class Settings extends React.Component {
             },
           });
         }
-        // return Promise.reject(new SubmissionError(e));
       });
   };
 
@@ -279,6 +336,42 @@ class Settings extends React.Component {
                 {this.context.translate(this.state.modal.error)}
               </Alert>
             )}
+            <FlicAction
+              url="/buttons/onClick"
+              actionText="On Click"
+              token={this.state.modal.token}
+            />
+            <FlicAction
+              url="/buttons/onDoubleClick"
+              actionText="On Hold"
+              token={this.state.modal.token}
+            />
+            <FlicAction
+              url="/buttons/onHold"
+              actionText="On Double Click"
+              token={this.state.modal.token}
+            />
+
+            <Row>
+              <Col className="text-center mb-2 mt-2">
+                <span className="btn-link">
+                  <a
+                    href="https://start.flic.io/"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    How to configure Flic Hub
+                  </a>
+                </span>
+              </Col>
+            </Row>
+            <Row>
+              <Col className="mb-2 mt-2">
+                <small>
+                  <b>Assign Flic button</b>
+                </small>
+              </Col>
+            </Row>
             <ScrollingEmployees
               size={80}
               list={this.state.modal.employees}
@@ -287,14 +380,19 @@ class Settings extends React.Component {
             />
           </ModalBody>
           <ModalFooter>
+            <Button
+              className="mr-auto"
+              color="danger"
+              onClick={() => this.deleteFlic(this.state.modal.id)}
+            >
+              Delete
+            </Button>
             <Button color="secondary" onClick={this.onFlicTileClick}>
               Cancel
             </Button>
             <Button
               color="primary"
-              onClick={() => {
-                this.updateFlicButton(this.state.modal);
-              }}
+              onClick={() => this.updateFlicButton(this.state.modal)}
               disabled={
                 this.state.modal.selectedEmployee ===
                 this.state.modal.originalEmployee
@@ -312,6 +410,64 @@ class Settings extends React.Component {
             </CardHeader>
             <CardBody>
               <br />
+              <Row>
+                <Col xs={12} className="mb-3">
+                  <ToggleSettingText
+                    settingName="general"
+                    settingKey="storeInfo"
+                    header="Store settings"
+                    description="Provide store information"
+                    value={this.state.settings.generalSettings.storeInfo}
+                    toggleSetting={(name, key, value) =>
+                      this.saveGeneralSettings({ storeInfo: value })
+                    }
+                  />
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={12} className="mb-3">
+                  <ToggleSettingText
+                    settingName="general"
+                    settingKey="address"
+                    header="Address"
+                    description="Location address"
+                    value={this.state.settings.generalSettings.address}
+                    toggleSetting={(name, key, value) =>
+                      this.saveGeneralSettings({ address: value })
+                    }
+                  />
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={12} className="mb-3">
+                  <ToggleSettingText
+                    settingName="general"
+                    settingKey="socials"
+                    header="Socials"
+                    description="Links to social networks"
+                    value={this.state.settings.generalSettings.socials}
+                    toggleSetting={(name, key, value) =>
+                      this.saveGeneralSettings({ socials: value })
+                    }
+                  />
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={12} className="mb-3">
+                  {process.env.BROWSER && (
+                    <EditorModal
+                      header="Terms & Conditions"
+                      description="Edit Terms and Conditions"
+                      value={this.state.settings.generalSettings.terms}
+                      savedState={this.state.terms}
+                      onChange={editorState =>
+                        this.setState({ terms: editorState })
+                      }
+                      save={() => this.saveTerms({ terms: this.state.terms })}
+                    />
+                  )}
+                </Col>
+              </Row>
               <Row>
                 <Col xs={{ size: 12 }}>
                   <GeneralSettings
@@ -385,7 +541,9 @@ class Settings extends React.Component {
                         settingKey="mobilePhoneVerification"
                         description="Client must verify there mobile phone"
                         header="Require mobile phone verification"
-                        checked={this.state.settings.accounts.mobilePhoneVerification}
+                        checked={
+                          this.state.settings.accounts.mobilePhoneVerification
+                        }
                         toggleSetting={this.toggleSetting}
                       />
                     </div>
@@ -403,9 +561,11 @@ class Settings extends React.Component {
                       <ToggleSetting
                         settingName="accounts"
                         settingKey="deleteUnusedAccounts"
-                        description="Automatically delete unused accounts"
-                        header="Delete unused accounts"
-                        checked={this.state.settings.accounts.deleteUnusedAccounts}
+                        description="Automatically delete unused client accounts"
+                        header="Delete unused client accounts"
+                        checked={
+                          this.state.settings.accounts.deleteUnusedAccounts
+                        }
                         toggleSetting={this.toggleSetting}
                       />
                     </div>
@@ -535,48 +695,48 @@ class Settings extends React.Component {
                     toggleSetting={this.toggleSetting}
                   />
                 </Col>
-                {/*<Col xs={12} className="mb-3">*/}
-                {/*  <ToggleSetting*/}
-                {/*    settingName="appointments"*/}
-                {/*    settingKey="autoConfirm"*/}
-                {/*    header="Automatically confirm all bookings"*/}
-                {/*    checked={this.state.settings.appointments.autoConfirm}*/}
-                {/*    toggleSetting={this.toggleSetting}*/}
-                {/*    disabled*/}
-                {/*  />*/}
-                {/*</Col>*/}
+                {/* <Col xs={12} className="mb-3"> */}
+                {/*  <ToggleSetting */}
+                {/*    settingName="appointments" */}
+                {/*    settingKey="autoConfirm" */}
+                {/*    header="Automatically confirm all bookings" */}
+                {/*    checked={this.state.settings.appointments.autoConfirm} */}
+                {/*    toggleSetting={this.toggleSetting} */}
+                {/*    disabled */}
+                {/*  /> */}
+                {/* </Col> */}
                 <Col xs={12} className="mb-3">
                   <ToggleSetting
                     settingName="appointments"
                     settingKey="notificationSlackPublic"
                     header="Send message to public channel when an appointments created/updated/canceled"
                     description="If enabled information about all appointments will be send to a public channel in Slack"
-                    checked={this.state.settings.appointments.notificationSlackPublic}
+                    checked={
+                      this.state.settings.appointments.notificationSlackPublic
+                    }
                     toggleSetting={this.toggleSetting}
                   />
                 </Col>
-                {/*<Col xs={12} className="mb-3">*/}
-                {/*  <ToggleSettingDropdown*/}
-                {/*    options={*/}
-                {/*      this.state.settings.timeRanges.from_1hour_to_12hour*/}
-                {/*    }*/}
-                {/*    settingName="appointments"*/}
-                {/*    isClearable*/}
-                {/*    placeholder="Do not move"*/}
-                {/*    settingKey="showInWaitingListTime"*/}
-                {/*    header="How many hours prior bookings will show up in waiting list"*/}
-                {/*    description="Amount of time required for a booking to appear in waiting list"*/}
-                {/*    value={*/}
-                {/*      this.state.settings.appointments.showInWaitingListTime*/}
-                {/*    }*/}
-                {/*    toggleSetting={this.toggleSetting}*/}
-                {/*  />*/}
-                {/*</Col>*/}
+                {/* <Col xs={12} className="mb-3"> */}
+                {/*  <ToggleSettingDropdown */}
+                {/*    options={ */}
+                {/*      this.state.settings.timeRanges.from_1hour_to_12hour */}
+                {/*    } */}
+                {/*    settingName="appointments" */}
+                {/*    isClearable */}
+                {/*    placeholder="Do not move" */}
+                {/*    settingKey="showInWaitingListTime" */}
+                {/*    header="How many hours prior bookings will show up in waiting list" */}
+                {/*    description="Amount of time required for a booking to appear in waiting list" */}
+                {/*    value={ */}
+                {/*      this.state.settings.appointments.showInWaitingListTime */}
+                {/*    } */}
+                {/*    toggleSetting={this.toggleSetting} */}
+                {/*  /> */}
+                {/* </Col> */}
                 <Col xs={12} className="mb-3">
                   <ToggleSettingDropdown
-                    options={
-                      this.state.settings.timeRanges.from_1hour_to_6hour
-                    }
+                    options={this.state.settings.timeRanges.from_1hour_to_6hour}
                     settingName="appointments"
                     isClearable
                     placeholder="Do not send"
@@ -629,8 +789,34 @@ class Settings extends React.Component {
                     settingKey="showOnTvOnlyTodayRecords"
                     header="Show only today's appointments on the screen"
                     description="If enabled only today's appointments will appear on TV Screens"
-                    checked={this.state.settings.appointments.showOnTvOnlyTodayRecords}
+                    checked={
+                      this.state.settings.appointments.showOnTvOnlyTodayRecords
+                    }
                     toggleSetting={this.toggleSetting}
+                  />
+                </Col>
+              </Row>
+            </CardBody>
+          </Card>
+        </Col>
+        <Col xs={{ size: 12 }}>
+          <Card>
+            <CardHeader>
+              <i className="icon-settings" />
+              Notifications
+            </CardHeader>
+            <CardBody>
+              <Row>
+                <Col xs={12} className="mb-3">
+                  <ToggleSettingText
+                    settingName="slack"
+                    settingKey="config"
+                    header="Slack settings"
+                    description="Slack workspace and channels settings"
+                    value={this.state.settings.slack.config}
+                    toggleSetting={(name, key, value) =>
+                      this.saveSettings({ 'slack.config': value })
+                    }
                   />
                 </Col>
               </Row>
@@ -646,6 +832,13 @@ class Settings extends React.Component {
             <CardHeader>
               <i className="icon-tag" />
               Flic buttons
+              <a
+                href="https://flic.io/"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <i className="icon-question pl-2" />
+              </a>
               <ToggleSetting
                 settingName="buttons"
                 settingKey="enabled"
@@ -670,9 +863,19 @@ class Settings extends React.Component {
                             id={el.id}
                             username={el.username}
                             name={el.name}
+                            token={el.token}
                           />
                         </Col>
                       ))}
+                  </Row>
+                  <Row>
+                    <Col>
+                      <div className="text-center">
+                        <span className="btn-link" onClick={this.addFlicButton}>
+                          Add
+                        </span>
+                      </div>
+                    </Col>
                   </Row>
                 </Col>
               </Row>
