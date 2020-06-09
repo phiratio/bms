668ad28e9aaa1e96989ead9d6761f5e5a16769cc
Alex Kulikovskikh <alex.kulikovskikh@gmail.com>
2020-06-09 16:16:41 -0400

    fix(backend): bootstrap script for disposable email service

--- backend/api/email/bootstrap/bootstrap.js
+++ backend/api/email/bootstrap/bootstrap.js
@@ -3,7 +3,7 @@
 module.exports = {
   initialize: async () => {
     const disposableEmails = await strapi.services.email.disposable.get();
-    if (disposableEmails.length === 0) {
+    if (!disposableEmails || disposableEmails.length === 0) {
       strapi.log.info("Refreshing list of disposable emails");
       await strapi.services.email.disposable.refreshList();
     }
